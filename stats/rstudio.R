f4c61326bee5e25c6ce0e3174614d1eb73aced83
damiengo <dam.gou@gmail.com>
2014-07-23 22:18:00 +0200

    Seasons goal diff avg

--- stats/rstudio.R
+++ stats/rstudio.R
@@ -1,3 +1,5 @@
+# Open file
+season_avg <- read.csv(file.choose())
 # Home avg
 plot(season_avg$start, season_avg$homeavg)
 lines(season_avg$start, season_avg$homeavg, type="l", col="blue")
@@ -7,3 +9,8 @@ lines(season_avg$start, season_avg$awayavg, type="l", col="blue")
 # Draw avg
 plot(season_avg$start, season_avg$drawavg)
 lines(season_avg$start, season_avg$drawavg, type="l", col="blue")
+# Open goal avg file
+goals_avg <- read.csv(file.choose())
+# Goal diff avg
+plot(goals_avg$start, goals_avg$diff_avg)
+lines(goals_avg$start, goals_avg$diff_avg, type="l", col="blue")
---
+++ stats/season_goals_diff_average.sql
@@ -0,0 +1,5 @@
+﻿SELECT s.start, ROUND((SUM(GREATEST(g.home_goals, g.away_goals)::float - LEAST(g.home_goals, g.away_goals)::float)/COUNT(g.*))::numeric, 2) AS diff_avg
+FROM games g
+INNER JOIN seasons s ON g.season_id = s.id
+GROUP BY s.start
+ORDER BY s.start
