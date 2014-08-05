884e68c8c6a245da1c47596d590a0a8ba07c3e1f
damiengo <dam.gou@gmail.com>
2014-08-05 18:06:05 +0200

    New stats

--- stats/season_goals_diff_average.sql
+++ stats/season_goals_diff_average.sql
@@ -1,4 +1,5 @@
-﻿SELECT s.start, ROUND((SUM(GREATEST(g.home_goals, g.away_goals)::float - LEAST(g.home_goals, g.away_goals)::float)/COUNT(g.*))::numeric, 2) AS diff_avg
+﻿SELECT s.start, 
+       ROUND((SUM(GREATEST(g.home_goals, g.away_goals)::float - LEAST(g.home_goals, g.away_goals)::float)/COUNT(g.*))::numeric, 2) AS diff_avg
 FROM games g
 INNER JOIN seasons s ON g.season_id = s.id
 GROUP BY s.start
