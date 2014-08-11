32da22bfba28638b6657fb58c70ec31988c415f3
damiengo <dam.gou@gmail.com>
2014-08-11 23:02:05 +0200

    rstudio

--- stats/season_n_np1.R
+++ stats/season_n_np1.R
@@ -2,5 +2,16 @@
 season <- read.csv(file.choose())
 # Plots
 plot(season$n_win, season$np1_win)
+wins <- lm(season$n_win ~ season$np1_win)
+abline(wins)
+summary(wins)
+
 plot(season$n_draw, season$np1_draw)
+draws <- lm(season$n_draw ~ season$np1_draw)
+abline(draws)
+summary(draws)
+
 plot(season$an_lose, season$np1_lose)
+losts <- lm(season$an_lose ~ season$np1_lose)
+abline(losts)
+summary(losts)
