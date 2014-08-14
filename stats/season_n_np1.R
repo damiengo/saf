aaa872e2a004cacbcb5829a106fad5f6ba62566a
damiengo <dam.gou@gmail.com>
2014-08-14 17:35:48 +0200

    rtudio

--- stats/season_n_np1.R
+++ stats/season_n_np1.R
@@ -1,26 +1,45 @@
 # Open file
-season <- read.csv(file.choose())
+season <- read.csv(file.choose(), encoding="UTF-8")
 # Plots
 plot(season$n_win, season$np1_win)
 wins <- lm(season$n_win ~ season$np1_win)
 abline(wins)
-summary(wins)
+# Correlation
+cor(season$n_win, season$np1_win)
+# Determination
+summary(wins)$r.squared
 
 plot(season$n_draw, season$np1_draw)
 draws <- lm(season$n_draw ~ season$np1_draw)
 abline(draws)
-summary(draws)
+# Correlation
+cor(season$n_draw, season$np1_draw)
+# Determination
+summary(draws)$r.squared
 
 plot(season$an_lose, season$np1_lose)
 losts <- lm(season$an_lose ~ season$np1_lose)
 abline(losts)
-summary(losts)
+# Correlation
+cor(season$an_lose, season$np1_lose)
+# Determination
+summary(losts)$r.squared
 
 # Function
-topTeams <- function(data) {
+top_teams <- function(data) {
   team_list <- unique(data$name)
-  return team_list
+  for(team in team_list) {
+    print("=======================")
+    team_seasons <- subset(data, name == team)
+    team_cor <- cor(team_seasons$n_win,team_seasons$np1_win)
+    print(team)
+    print(team_cor)
+    team_wins <- lm(team_seasons$n_win ~ team_seasons$np1_win)
+    print("=======================")
+  }
+  
+  return(team_list)
 }
 
-list <- topTeams(season)
+list <- top_teams(season)
 
