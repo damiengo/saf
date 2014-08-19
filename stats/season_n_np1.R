99981e7179ee1618023432370e0bf61d8fe294fb
damiengo <dam.gou@gmail.com>
2014-08-19 22:35:36 +0200

    Updated stats

--- stats/season_n_np1.R
+++ stats/season_n_np1.R
@@ -1,5 +1,6 @@
 # Open file
 season <- read.csv(file.choose(), encoding="UTF-8")
+season <- subset(season, season_n > 1993)
 # Plots
 plot(season$n_win, season$np1_win)
 wins <- lm(season$n_win ~ season$np1_win)
@@ -27,16 +28,18 @@ summary(losts)$r.squared
 
 # Function
 top_teams <- function(data) {
-  file_name <- "n_np1_score/losts_teams_cor_from_1993.csv"
+  file_name <- "n_np1_score/wins_teams_cor_from_1993.csv"
   if (file.exists(file_name)) file.remove(file_name)
   team_list <- unique(data$name)
   for(team in team_list) {
     print("=======================")
     team_seasons <- subset(data, name == team & season_n > 1993)
-    team_cor <- cor(team_seasons$an_lose,team_seasons$np1_lose)
+    team_cor <- cor(team_seasons$n_win,team_seasons$np1_win)
+    team_lm  <- summary(lm(team_seasons$n_win ~ team_seasons$np1_win))$r.squared
     print(team)
     print(team_cor)
-    write.table(list(team, team_cor), file = file_name, col.names=FALSE, sep=",", append=TRUE)
+    print(team_lm)
+    write.table(list(team, team_cor,team_lm), file = file_name, col.names=FALSE, sep=",", append=TRUE)
     print("=======================")
   }
   
@@ -44,4 +47,3 @@ top_teams <- function(data) {
 }
 
 list <- top_teams(season)
-
