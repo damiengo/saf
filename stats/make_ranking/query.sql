cba8dd29f1dad4b332ec07d51087ecc6373e5269
damiengo <dam.gou@gmail.com>
2014-09-14 22:22:48 +0200

    Ranking OK

--- stats/make_ranking/query.sql
+++ stats/make_ranking/query.sql
@@ -1,42 +1,9 @@
-﻿SELECT * 
-FROM
-(
-SELECT DISTINCT g.home_team_id, g.day
-FROM games g 
-INNER JOIN seasons s ON g.season_id = s.id
-WHERE s.start = 2013
-ORDER BY g.day ASC
-) teams_days
-
-
-SELECT SUM(
-       CASE
-       WHEN g.home_team_id = 30 THEN
-           CASE 
-               WHEN g.home_goals > g.away_goals THEN 3
-               WHEN g.home_goals = g.away_goals THEN 1
-               WHEN g.home_goals < g.away_goals THEN 0
-           END
-       ELSE
-           CASE 
-               WHEN g.home_goals > g.away_goals THEN 0
-               WHEN g.home_goals = g.away_goals THEN 1
-               WHEN g.home_goals < g.away_goals THEN 3
-           END
-        END)
-FROM games g
-INNER JOIN seasons s ON g.season_id = s.id
-WHERE s.start = 2013
-AND (g.home_team_id = 30 OR g.away_team_id = 30)
-AND g.day <= 8
-
-
-SELECT s.start, 
+﻿SELECT s.start, 
        teams_day.day,
        t.name,
        SUM(
 	       CASE
-	       WHEN g.home_team_id = teams_day.home_team_id THEN
+	       WHEN g.home_team_id = teams_day.team_id THEN
 		   CASE 
 		       WHEN g.home_goals > g.away_goals THEN 3
 		       WHEN g.home_goals = g.away_goals THEN 1
@@ -49,19 +16,23 @@ SELECT s.start,
 		       WHEN g.home_goals < g.away_goals THEN 3
 		   END
 		END
-	)
+	) AS points
 FROM games g
 INNER JOIN seasons s ON g.season_id = s.id
 INNER JOIN
 (
-	SELECT DISTINCT g.home_team_id, g.day
+	SELECT DISTINCT g.home_team_id AS team_id, g.day
 	FROM games g 
 	INNER JOIN seasons s ON g.season_id = s.id
 	WHERE s.start = 2013
-	ORDER BY g.day ASC
-) teams_day ON (g.home_team_id = teams_day.home_team_id OR g.away_team_id = teams_day.home_team_id)
-INNER JOIN teams t ON teams_day.home_team_id = t.id
+	UNION
+	SELECT DISTINCT g.away_team_id AS team_id, g.day
+	FROM games g 
+	INNER JOIN seasons s ON g.season_id = s.id
+	WHERE s.start = 2013
+) teams_day ON (g.home_team_id = teams_day.team_id OR g.away_team_id = teams_day.team_id)
+INNER JOIN teams t ON teams_day.team_id = t.id
 WHERE s.start = 2013 
 AND g.day <= teams_day.day
 GROUP BY s.start, teams_day.day, t.name
-ORDER BY s.start, teams_day.day
+ORDER BY s.start ASC, teams_day.day ASC, points DESC
