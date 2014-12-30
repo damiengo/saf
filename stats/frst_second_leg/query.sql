d19f3153f8776622104b8177792f8c53cb19c260
damiengo <dam.gou@gmail.com>
2014-12-30 18:27:36 +0100

    Rank/points

--- stats/frst_second_leg/query.sql
+++ stats/frst_second_leg/query.sql
@@ -7,7 +7,27 @@ SELECT first.start,
        second.away_goals AS second_t1, 
        second.home_goals AS second_t2, 
        first_team_rank.rank AS t1_rank, 
-       second_team_rank.rank AS t2_rank
+       second_team_rank.rank AS t2_rank, 
+       CASE 
+       WHEN first.home_goals > first.away_goals THEN 3
+       WHEN first.home_goals = first.away_goals THEN 1
+       WHEN first.home_goals < first.away_goals THEN 0
+       END AS result_first_t1, 
+       CASE 
+       WHEN first.home_goals < first.away_goals THEN 3
+       WHEN first.home_goals = first.away_goals THEN 1
+       WHEN first.home_goals > first.away_goals THEN 0
+       END AS result_first_t2, 
+       CASE 
+       WHEN second.home_goals < second.away_goals THEN 3
+       WHEN second.home_goals = second.away_goals THEN 1
+       WHEN second.home_goals > second.away_goals THEN 0
+       END AS result_second_t1, 
+       CASE 
+       WHEN second.home_goals > second.away_goals THEN 3
+       WHEN second.home_goals = second.away_goals THEN 1
+       WHEN second.home_goals < second.away_goals THEN 0
+       END AS result_second_t2
 FROM
 (
 SELECT s.id AS season_id,  s.start, g.day, ht.id AS home_team_id, ht.name as home, at.id AS away_team_id, at.name as away, g.home_goals, g.away_goals
@@ -15,7 +35,7 @@ FROM games g
 INNER JOIN seasons s ON g.season_id = s.id AND s.start >= 2002
 INNER JOIN teams ht ON g.home_team_id = ht.id
 INNER JOIN teams at ON g.away_team_id = at.id
-WHERE g.day <= 18
+WHERE g.day <= 19
 ) first, 
 (
 SELECT s.id AS season_id, s.start, g.day, ht.id AS home_team_id, ht.name as home, at.name as away, g.home_goals, g.away_goals
@@ -23,17 +43,17 @@ FROM games g
 INNER JOIN seasons s ON g.season_id = s.id AND s.start >= 2002
 INNER JOIN teams ht ON g.home_team_id = ht.id
 INNER JOIN teams at ON g.away_team_id = at.id
-WHERE g.day > 18
+WHERE g.day > 19
 ) second, 
 (
 SELECT r.season_id, r.day, r.team_id, r.rank
 FROM ranking3pts r
-WHERE r.day = 18
+WHERE r.day = 19
 ) first_team_rank, 
 (
 SELECT r.season_id, r.day, r.team_id, r.rank
 FROM ranking3pts r
-WHERE r.day = 18
+WHERE r.day = 19
 ) second_team_rank
 WHERE first.start = second.start
 AND first.home = second.away
--- stats/frst_second_leg/script.R
+++ stats/frst_second_leg/script.R
@@ -1,12 +1,18 @@
 results = read.csv("first_second_leg.csv", sep=";", encoding="UTF-8")
 
+# Goals diff
 first_results = results$first_t1 - results$first_t2
 second_results = results$second_t1 - results$second_t2
-
+cor(first_results, second_results)
 goals_diff = lm(first_results ~ second_results)
-
 summary(goals_diff)
-
 plot(jitter(first_results) ~ jitter(second_results))
-
 abline(goals_diff)
+
+# Rank diff/points
+rank_diff   = results$t1_rank - results$t2_rank
+points_diff = (results$result_first_t1+results$result_second_t1) - (results$result_first_t2+results$result_second_t2)
+rank_point_diff = lm(points_diff ~ rank_diff)
+summary(rank_point_diff)
+plot(points_diff ~ rank_diff)
+abline(rank_point_diff)
