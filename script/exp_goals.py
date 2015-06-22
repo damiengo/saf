9181dee172a3d9014c365ea7276355c6d625f461
damiengo <dam.gou@gmail.com>
2015-06-22 22:58:53 +0200

    Logistic

--- script/exp_goals.py
+++ script/exp_goals.py
@@ -2,14 +2,15 @@
 
 from sklearn import cross_validation
 from sklearn.linear_model import LogisticRegression
+from numpy import genfromtxt, loadtxt, asarray
 
 def main():
     print("==== START ====")
     
-    dataset = loadtxt('../data/stats/shots_position_bead.tsv', delimiter='\t', dtype='f8', skiprows=1)
+    dataset = loadtxt('../data/stats/shots_position_head_2014.tsv', delimiter='\t', dtype='f8', skiprows=1)
     target = [x[0] for x in dataset]
     train = [x[1:] for x in dataset]
-    test = genfromtxt(open('../data/stats/shots_position_head.tsv','r'), delimiter='\t', dtype='f8', skiprows=1)
+    test = genfromtxt(open('../data/stats/shots_position_head_2014.tsv','r'), delimiter='\t', dtype='f8', skip_header=1)
     
     model = LogisticRegression()
     model = model.fit(train, target)
@@ -17,6 +18,9 @@ def main():
     kf_total = cross_validation.KFold(len(train), n_folds=2)
     scores = cross_validation.cross_val_score(model, asarray(train), asarray(target), cv=kf_total, n_jobs=1)
 
-    print("Accuracy of %i: %0.2f (+/- %0.2f)" % (nb_train, scores.mean(), scores.std() * 2))
+    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
     
     print("==== END ====")
+
+if __name__=="__main__":
+    main()
--- stats/expg/expg.sql
+++ stats/expg/expg.sql
@@ -1,8 +1,7 @@
-SELECT CASE WHEN gae.event_type = 'goal' THEN 1 ELSE 0 END as goal,
-       gae.event_type, 
+SELECT CASE WHEN gae.event_type = 'goal' THEN 1 ELSE 0 END AS goal,
        gae.mins, 
        gae.secs, 
        gae.start_x, 
        gae.start_y, 
-       gae.headed
+       CASE WHEN gae.headed THEN 1 ELSE 0 END AS headed
 FROM sqw_goals_attempts_events gae
