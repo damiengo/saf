0ae92cd6c295a39a94c79dd22e22e5d78482d3e1
damiengo <dam.gou@gmail.com>
2015-07-26 00:39:49 +0200

    Train

--- script/exp_goals_teams.py
+++ script/exp_goals_teams.py
@@ -9,36 +9,35 @@ def main():
     print("==== START ====")
 
     # start	short_name	goal	start_x	start_y	headed
-    #dataset = loadtxt('../data/stats/shots_position_head_teams_2012_2014.tsv',
-    dataset = genfromtxt('../data/stats/shots_position_head_teams_2012_2014.tsv',
-                      delimiter='\t',
-                      usecols=(2,3,4,5),
-                      dtype='f8',
-                      #dtype={'names':   ('start', 'short_name', 'goal', 'start_x', 'start_y', 'headed'),
-                      #'formats': ('i4', 'S30', 'i4', 'f8', 'f8', 'i4')},
-                      skip_header=1)
     dataset = DataFrame.from_csv('../data/stats/shots_position_head_teams_2012_2014.tsv', sep='\t', index_col=False)
-
-    target = [x[2] for x in dataset]
-    train = [x[3:] for x in dataset]
 
     target = dataset.loc[:,'goal']
     train = dataset.loc[:,['start_x', 'start_y', 'headed']]
 
-    test = genfromtxt(open('../data/stats/shots_position_head_teams_2012_2014.tsv','r'), delimiter='\t', dtype='f8', skip_header=1)
+
+    train_target   = target.head(10000)
+    train_features = train.head(10000)
+
+    dataset_test = dataset.tail(5000)
+    test_target = target.tail(5000)
+    test_features = train.tail(5000)
+
+    dataset_test = dataset_test.reset_index(drop=True)
+    test_target = test_target.reset_index(drop=True)
+    test_features = test_features.reset_index(drop=True)
 
     model = LogisticRegression()
-    model = model.fit(train, target)
+    model = model.fit(test_features, test_target)
 
-    kf_total = cross_validation.KFold(len(train), n_folds=2)
-    scores = cross_validation.cross_val_score(model, asarray(train), asarray(target), cv=kf_total, n_jobs=1)
+    kf_total = cross_validation.KFold(len(test_features), n_folds=2)
+    scores = cross_validation.cross_val_score(model, asarray(test_features), asarray(test_target), cv=kf_total, n_jobs=1)
 
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
-    predicted_probs = model.predict_proba(train)
+    predicted_probs = model.predict_proba(test_features)
     predicted_goals = DataFrame(predicted_probs[:,1], columns=['predict'])
 
-    results = concat([dataset, predicted_goals], axis=1)
+    results = concat([dataset_test, predicted_goals], axis=1)
     grouped_results = results.groupby(['start', 'short_name']).sum()
 
     grouped_results.to_csv('../data/stats/exp_goals_by_teams.tsv', sep='\t', encoding='utf-8')
--- script/exp_goals_teams_v2.py
+++ script/exp_goals_teams_v2.py
@@ -2,43 +2,42 @@
 
 from sklearn import cross_validation
 from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.ensemble import RandomForestClassifier
 from numpy import genfromtxt, loadtxt, asarray, savetxt, c_, newaxis
 from pandas import DataFrame, read_csv, concat
 
 def main():
     print("==== START ====")
 
-    # start	short_name	goal	start_x	start_y	headed
-    #dataset = loadtxt('../data/stats/shots_position_head_teams_2012_2014.tsv',
-    dataset = genfromtxt('../data/stats/shots_teams_2013_2014.tsv',
-                      delimiter='\t',
-                      usecols=(2,3,4,5,6,7,8,9,10),
-                      dtype='f8',
-                      #dtype={'names':   ('start', 'short_name', 'goal', 'start_x', 'start_y', 'headed'),
-                      #'formats': ('i4', 'S30', 'i4', 'f8', 'f8', 'i4')},
-                      skip_header=1)
     dataset = DataFrame.from_csv('../data/stats/shots_teams_2013_2014.tsv', sep='\t', index_col=False)
-
-    target = [x[2] for x in dataset]
-    train = [x[3:] for x in dataset]
 
     target = dataset.loc[:,'goal']
     train = dataset.loc[:,['degree', 'distance', 'shot_headed', 'crosses', 'corner', 'pass_throw_in', 'pass_long_ball', 'pass_through_ball', 'pass_headed']]
 
-    test = genfromtxt(open('../data/stats/shots_teams_2013_2014.tsv','r'), delimiter='\t', dtype='f8', skip_header=1)
+    train_target   = target.head(10000)
+    train_features = train.head(10000)
+
+    dataset_test = dataset.tail(5000)
+    test_target = target.tail(5000)
+    test_features = train.tail(5000)
+
+    dataset_test = dataset_test.reset_index(drop=True)
+    test_target = test_target.reset_index(drop=True)
+    test_features = test_features.reset_index(drop=True)
 
     model = LogisticRegression()
-    model = model.fit(train, target)
+    #model = RandomForestClassifier(n_estimators = 100)
+    model = model.fit(train_features, train_target)
 
-    kf_total = cross_validation.KFold(len(train), n_folds=2)
-    scores = cross_validation.cross_val_score(model, asarray(train), asarray(target), cv=kf_total, n_jobs=1)
+    kf_total = cross_validation.KFold(len(test_features), n_folds=2)
+    scores = cross_validation.cross_val_score(model, asarray(test_features), asarray(test_target), cv=kf_total, n_jobs=1)
 
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
-    predicted_probs = model.predict_proba(train)
+    predicted_probs = model.predict_proba(test_features)
     predicted_goals = DataFrame(predicted_probs[:,1], columns=['predict'])
 
-    results = concat([dataset, predicted_goals], axis=1)
+    results = concat([dataset_test, predicted_goals], axis=1)
     grouped_results = results.groupby(['start', 'short_name']).sum()
 
     grouped_results.to_csv('../data/stats/exp_goals_v2_by_teams.tsv', sep='\t', encoding='utf-8')
@@ -52,7 +51,7 @@ def main():
     print predicts
     reg = lr.fit(predicts[:,newaxis], goals)
     print "R2: "+"%.9f" % reg.score(predicts[:,newaxis], goals)
-    print reg.predict(predicts[:,newaxis])
+    #print reg.predict(predicts[:,newaxis])
 
     #savetxt('../data/stats/exp_goals.tsv', results, delimiter='\t', fmt='%f', header="x\ty\thead\tpredict")
 
