2ac0a166b1873b96933af108ecf0cdac0c237dd8
damiengo <dam.gou@gmail.com>
2017-01-23 22:31:53 +0100

    538 scrap

--- app/controllers/bet_controller.rb
+++ app/controllers/bet_controller.rb
@@ -4,12 +4,24 @@ class BetController < ApplicationController
     # Predictions
 
     url = "https://projects.fivethirtyeight.com/soccer-predictions/ligue-1/"
-    #File.open("/tmp/538.html", 'w') { |file| file.write(session.html) }
     resp = Net::HTTP.get_response(URI.parse(url))
     data = resp.body
+
+    # File.open("/tmp/538.html", 'w:UTF-8') { |file| file.write(data.encode('UTF-8', {
+    #   :invalid => :replace,
+    #   :undef   => :replace,
+    #   :replace => '?'
+    # })) }
+
     first_page = Nokogiri::HTML(data, nil, "utf-8")
     first_page.css(".match-container").each do |match|
-      puts match.css(".match-top")
+      home_team = match.css(".match-top .team-div .name").text
+      away_team = match.css(".match-bottom .team-div .name").text
+      home_prob = match.css(".match-top .prob:not(.tie-prob)").text.chop
+      away_prob = match.css(".match-bottom .prob:not(.tie-prob)").text.chop
+      tie_prob  = match.css(".match-top .tie-prob").text.chop
+
+      puts home_team + " - " + away_team + ": " + home_prob.to_s + "/" + tie_prob.to_s + "/" + away_prob.to_s
     end
 
     # Odds
