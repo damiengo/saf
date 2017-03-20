9f0ed61db15aa605f479dd43c61c34040b0f9220
damiengo <dam.gou@gmail.com>
2017-03-20 22:04:10 +0100

    gp

--- script/kaggle_data/project/graph_data/entropies.py
+++ script/kaggle_data/project/graph_data/entropies.py
@@ -8,30 +8,28 @@ class Entropies:
 
     def mean_by_league(self):
         print "mean by league"
-        grouped_means = self.df.groupby(['league', 'season'])['sum_proba_b365',
-                                                    'sum_proba_bw',
-                                                    'sum_proba_iw',
-                                                    'sum_proba_lb',
-                                                    'sum_proba_ps',
-                                                    'sum_proba_wh',
-                                                    'sum_proba_sj',
-                                                    'sum_proba_vc',
-                                                    'sum_proba_gb',
-                                                    'sum_proba_bs'].mean()
+        providers = ['sum_proba_b365', 'sum_proba_bw', 'sum_proba_iw',
+                     'sum_proba_lb', 'sum_proba_ps', 'sum_proba_wh',
+                     'sum_proba_sj', 'sum_proba_vc', 'sum_proba_gb',
+                     'sum_proba_bs']
+        grouped_means = self.df.groupby(['league', 'season'])[providers].mean()
+
+        datas = grouped_means['sum_proba_b365']
+        datas = datas.reset_index().pivot(index='season', columns='league')
+
         #plot graph
-        ax = grouped_means.plot(figsize=(12,8),marker='o')
+        ax = datas.plot(figsize=(12,8), style=["-","--","-.",":"], marker='o')
 
         #set title
-        plt.title('Leagues Predictability', fontsize=16)
+        plt.title('Probability mean sums', fontsize=16)
 
         #set ticks roatation
         plt.xticks(rotation=50)
 
         #keep colors for next graph
-        colors = [x.get_color() for x in ax.get_lines()]
-        #colors_mapping = dict(zip(leagues.id, colors))
+        #colors = [x.get_color() for x in ax.get_lines()]
 
         #remove x label
-        ax.set_xlabel('')
+        #ax.set_xlabel('')
 
         plt.show()
