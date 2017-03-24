c14ed005db65c4af6c26be0cca0736bada7e1c0e
damiengo <dam.gou@gmail.com>
2017-03-24 22:39:42 +0100

    means

--- script/kaggle_data/project/graph_data/entropies.py
+++ script/kaggle_data/project/graph_data/entropies.py
@@ -8,27 +8,34 @@ class Entropies:
 
     def mean_by_league(self):
         print "mean by league"
-        providers = ['sum_proba_b365', 'sum_proba_bw', 'sum_proba_iw',
-                     'sum_proba_lb', 'sum_proba_ps', 'sum_proba_wh',
-                     'sum_proba_sj', 'sum_proba_vc', 'sum_proba_gb',
-                     'sum_proba_bs']
-        grouped_means = self.df.groupby(['league', 'season'])[providers].mean()
+        providers = [('B365', 'sum_proba_b365'),
+                     ('BW', 'sum_proba_bw'),
+                     ('IW', 'sum_proba_iw'),
+                     ('LB', 'sum_proba_lb'),
+                     ('PS', 'sum_proba_ps'),
+                     ('WH', 'sum_proba_wh'),
+                     ('SJ', 'sum_proba_sj'),
+                     ('VC', 'sum_proba_vc'),
+                     ('GB', 'sum_proba_gb'),
+                     ('BS', 'sum_proba_bs')]
 
-        datas = grouped_means['sum_proba_b365']
-        datas = datas.reset_index().pivot(index='season', columns='league', values='sum_proba_b365')
+        for lib, sum_col in providers:
+            grouped_means = self.df.groupby(['league', 'season'])[sum_col].mean()
+            datas = grouped_means.reset_index().pivot(index='season', columns='league', values=sum_col)
 
-        #plot graph
-        ax = datas.plot(figsize=(12,8), style=["-","--","-.",":","-","--","-.",":","-","--","-.",":"], marker='o', linewidth=2.0)
-        fig = ax.get_figure()
-        fig.patch.set_facecolor('white')
+            #plot graph
+            ax = datas.plot(figsize=(12,8), style=["-","--","-.",":","-","--","-.",":","-","--","-.",":"], marker='o', linewidth=2.0)
+            fig = ax.get_figure()
+            fig.patch.set_facecolor('white')
 
-        #set title
-        hfont = {'fontname':'Helvetica'}
-        plt.title('PROBABILITY MEAN SUMS', fontsize=20, color='#444444', **hfont)
+            #set title
+            hfont = {'fontname':'Helvetica'}
+            plt.title('PROBABILITY MEAN SUMS '+lib, fontsize=20, color='#444444', **hfont)
 
-        ax.set_xlabel('')
+            ax.set_xlabel('')
 
-        #set ticks roatation
-        plt.xticks(rotation=50)
+            #set ticks roatation
+            plt.xticks(rotation=50)
 
-        plt.show()
+            plt.savefig('/home/dam/Images/sum_means/'+lib+'.png', facecolor='white')
+            #plt.show()
