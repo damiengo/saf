20c94e174aca5d5e8b9420310103e94414bc503b
damiengo <dam.gou@gmail.com>
2017-07-21 23:11:34 +0200

    calc expg

--- script/expg_nn/project/data_preparation/clean_data.py
+++ script/expg_nn/project/data_preparation/clean_data.py
@@ -12,6 +12,7 @@ class CleanData:
         df = df[df.own_goal == False]
 
         # Keep only necessary
+        """
         df = df[[  'goal', 'distance', 'degree', 'long_ball', 'through_ball', 'headed',
                    'on_corner', 'on_cross', 'on_pass', 'on_back_pass', 'on_back_cross', 'on_shot_save',
                    'on_shot_off', 'on_shot_block', 'on_shot_ww', 'on_interception', 'on_tackle', 'on_gk_failedcatch',
@@ -20,6 +21,7 @@ class CleanData:
                    'zone_10', 'zone_11', 'zone_12', 'zone_13', 'zone_14', 'zone_15', 'zone_16', 'zone_17', 'zone_18',
                    'n1_zone_10', 'n1_zone_11', 'n1_zone_12', 'n1_zone_13', 'n1_zone_14',
                    'n1_zone_15', 'n1_zone_16', 'n1_zone_17', 'n1_zone_18']]
+        """
 
         # Normalize values
         """
--- script/expg_nn/project/predict_tpot.py
+++ script/expg_nn/project/predict_tpot.py
@@ -13,26 +13,28 @@ if(len(sys.argv) < 3):
     print "Use: command <test> <predict>"
     sys.exit(2)
 
-test_file = sys.argv[1]
+test_file    = sys.argv[1]
 predict_file = sys.argv[2]
 
-test_shots = pd.read_csv(test_file)
+test_shots    = pd.read_csv(test_file)
 predict_shots = pd.read_csv(predict_file)
 
 # Cleaning
-cd = clean_data.CleanData()
-test_shots = cd.run(test_shots)
-predict_shots = cd.run(predict_shots)
+cd               = clean_data.CleanData()
+test_shots_cl    = cd.run(test_shots)
+predict_shots_cl = cd.run(predict_shots)
 
 # Train/test splitting
-tf = target_features.TargetFeatures()
-X_test, y_test = tf.run(test_shots)
-X, y = tf.run(predict_shots)
+tf             = target_features.TargetFeatures()
+#X_test, y_test = tf.run(test_shots_cl)
+#X, y           = tf.run(predict_shots_cl)
 
 # Predict
 clf = XGBClassifier(max_depth=2, min_child_weight=9, n_estimators=100, nthread=1, subsample=0.65)
-clf.fit(X_test, y_test)
-predicted = clf.predict_proba(X)
-print(predicted[:,1])
+clf.fit(test_shots_cl[tf.X], test_shots_cl[tf.y])
+predicted = clf.predict_proba(predict_shots_cl[tf.X])
+
+predict_shots_cl['expg'] = predicted[:,1]
+predict_shots_cl.to_csv('save/expg_prepared_data/predicted_expg.csv', index=False)
 
 log.info("END")
